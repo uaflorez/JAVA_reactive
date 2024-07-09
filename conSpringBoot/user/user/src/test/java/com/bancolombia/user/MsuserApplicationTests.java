f0cd7f6e3fc5fcd999babb1b2031ef1c719a9c50
uflorez <urielflorez29@gmail.com>
2024-07-09 17:38:08 -0500

    Actualización proyecto auto y mas

---
+++ conSpringBoot/user/user/src/test/java/com/bancolombia/user/MsuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancolombia.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
