826ec73f4f108e522a7a9bff11fa466201ccf89f
uflorez <urielflorez29@gmail.com>
2024-07-09 17:34:13 -0500

    Actualizacion proyecto auto

--- conSpringBoot/user/user/src/main/java/com/bancolombia/user/controller/UserController.java
+++ conSpringBoot/user/user/src/main/java/com/bancolombia/user/controller/UserController.java
@@ -1,2 +1,57 @@
-package com.bancolombia.user.controller;public class UserController {
+package com.bancolombia.user.controller;
+
+import com.bancolombia.user.entity.User;
+import com.bancolombia.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userService.getUserById(id);
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        Optional<User> existingUser = userService.getUserById(id);
+
+        if (existingUser.isPresent()) {
+            User user = existingUser.get();
+            user.setName(userDetails.getName());
+            user.setEmail(userDetails.getEmail());
+            User updatedUser = userService.saveUser(user);
+            return ResponseEntity.ok(updatedUser);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
--- conSpringBoot/user/user/src/main/java/com/bancolombia/user/entity/User.java
+++ conSpringBoot/user/user/src/main/java/com/bancolombia/user/entity/User.java
@@ -1,2 +1,50 @@
-package com.bancolombia.user.entity;public class User {
+package com.bancolombia.user.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "user")
+    private List<BankAccount> bankAccounts;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<BankAccount> getBankAccounts() {
+        return bankAccounts;
+    }
+
+    public void setBankAccounts(List<BankAccount> bankAccounts) {
+        this.bankAccounts = bankAccounts;
+    }
 }
